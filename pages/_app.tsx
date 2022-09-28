7577a0903a2bf13d4751d61e24d7a7bc8271ec2d
Pro-ktmr <Pro.ktmr@gmail.com>
2022-09-28 23:17:13 +0900

    Normalize css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import '../node_modules/modern-normalize/modern-normalize.css'
 import '../components/global.css'
 
 export default function App({ Component, pageProps }: AppProps) {
